654e6bbf602beb924b375e747d883d557d361bbf
leolarrel <>
2021-08-27 11:35:44 +0800

    Project start. KGI option trade TCP/IP daemon

---
+++ kgi_trade_daemon.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class ServiceMain
+{
+	public static int Main(String[] args)
+	{
+		string WelcomString = "KGI option trade TCP/IP Daemon";
+		Console.WriteLine(WelcomString);
+		return 0;
+	}
+}
+
